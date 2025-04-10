27538f43b135d2ea608956537069350d6203cadf
Nitish jhamb <nitishjhamb20@gmail.com>
2025-04-11 02:10:09 +0530

    normal commit

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "./dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name]-[hash][extname]",
-        entryFileNames: "assets/[name]-[hash].js",
-      },
-    },
-  },
+  base: "/ChatVaani/",
 });
