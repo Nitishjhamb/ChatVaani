f1dd486e32558541a49a8e3478425edab8f51260
Nitish jhamb <nitishjhamb20@gmail.com>
2025-04-11 01:27:17 +0530

    Test CI/CD pipeline 18

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  plugins: [react()],
   base: "/ChatVaani/",
+  build: {
+    outDir: "../dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
 });
